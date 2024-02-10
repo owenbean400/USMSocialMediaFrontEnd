a10bbb8553442a70ef45003dd419f2e580227362
Owen Bean <owenbean@Owens-MBP-98.lan>
2024-02-10 05:14:43 -0500

    First iteration of USM front end login page

---
+++ src/pages/login/login-page.js
@@ -0,0 +1,67 @@
+import './login-page.css';
+import React, { useState } from 'react';
+import TextFieldPassword from '../../components/inputs/usm-text-field-password';
+import TextFieldExtended from '../../components/inputs/usm-text-field-extended';
+import Usmbutton from '../../components/button/usm-button';
+import ConnectConfig from '../../config/connections.json';
+import { Link, useNavigate } from 'react-router-dom';
+
+function LoginPage() {
+  const [emailaddrInput, setEmailInput] = useState('');
+  const [passInput, setPassInput] = useState('');
+  const [errMessage, setErrMessage] = useState('');
+  const navigate = useNavigate()
+
+  function handleEmailChange(value) {
+    setEmailInput(value);
+  };
+
+  function handlePassChange(value) {
+    setPassInput(value);
+  };
+
+  async function clickOn() {
+    const URL = ConnectConfig.api_server.url + "/api/v1/auth/authenticate"
+
+    let credentials = {
+      email: emailaddrInput + "@maine.edu",
+      password: passInput
+    }
+
+    try {
+      const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+      });
+
+      if (response.ok) {
+        let data = await response.json();
+        sessionStorage.setItem(ConnectConfig.api_server.session_token_id_name, data.token);
+        navigate('/main');
+      } else {
+        setErrMessage('Login error!');
+      }
+    } catch (error) {
+      setErrMessage('Login error!');
+    }
+  }
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <img className="usm-logo-top" src="https://imgs.search.brave.com/EPO1Pfw_I9IxOnuFLTFMf_y9eR2TpuWsZ_5t7ADh1Qg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91c20u/bWFpbmUuZWR1L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIyLzA3/L1VTTV9oZWFkZXJM/b2dvLnBuZw"></img>
+        <p>{errMessage}</p>
+        <TextFieldExtended labelText="Email Address" labelExtenstion="@maine.edu" onChange={handleEmailChange}/>
+        <TextFieldPassword labelText=" Password" onChange={handlePassChange}/>
+        <Link className="login-links" to="/register">Register</Link>
+        <a className="login-links">Reset Password</a>
+        <Usmbutton buttonText="Login" onClick={clickOn}/>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/login/register-user.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import TextFieldPassword from '../../components/inputs/usm-text-field-password';
+import TextFieldExtended from '../../components/inputs/usm-text-field-extended';
+import Usmbutton from '../../components/button/usm-button';
+import ConnectConfig from '../../config/connections.json';
+import { Link, useNavigate } from "react-router-dom";
+
+function RegisterPage() {
+    const [emailaddrInput, setEmailInput] = useState('');
+    const [passInput, setPassInput] = useState('');
+    const [confirmPassInput, setConfirmPassInput] = useState('');
+    const [errMessage, setErrMessage] = useState('');
+    const navigate = useNavigate()
+
+    function handleEmailChange(value) {
+        setEmailInput(value);
+    };
+
+    function handlePassChange(value) {
+        setPassInput(value);
+    };
+
+    function handleConfirmPassChange(value) {
+        setConfirmPassInput(value);
+    };
+
+    async function registerAccount() {
+        const URL = ConnectConfig.api_server.url + "/api/v1/auth/register";
+
+        if (passInput !== confirmPassInput) {
+            setErrMessage("Password and confirm password does not match!");
+        }
+        
+        let credentials = {
+            email: emailaddrInput + "@maine.edu",
+            password: passInput
+        }
+
+        try {
+            const response = await fetch(URL, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(credentials)
+            });
+      
+            if (response.ok) {
+                let data = await response.json();
+                sessionStorage.setItem(ConnectConfig.api_server.session_token_id_name, data.token);
+                navigate('/main');
+            } else {
+                setErrMessage('Registration error!');
+            }
+          } catch (error) {
+            setErrMessage('Registration error!');
+          }
+    }
+
+    return (
+        <div className="login-page">
+          <div className="login-container">
+                <img className="usm-logo-top" src="https://imgs.search.brave.com/EPO1Pfw_I9IxOnuFLTFMf_y9eR2TpuWsZ_5t7ADh1Qg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91c20u/bWFpbmUuZWR1L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIyLzA3/L1VTTV9oZWFkZXJM/b2dvLnBuZw" width="380px"></img>
+                <p>{errMessage}</p>
+                <TextFieldExtended labelText="Email Address" labelExtenstion="@maine.edu" onChange={handleEmailChange}/>
+                <TextFieldPassword labelText="Password" onChange={handlePassChange}/>
+                <TextFieldPassword labelText="Confirm Password" onChange={handleConfirmPassChange}/>
+                <Usmbutton buttonText="Register" onClick={registerAccount}/>
+                <Link className="login-links" to="/">Back</Link>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterPage;
