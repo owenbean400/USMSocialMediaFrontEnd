e5ac6094ca3295c7a9f3d5ab03795c3047b0c653
Owen Bean <owenbean400@gmail.com>
2024-02-17 13:34:57 -0500

    Pretty up verification pages

--- src/pages/login/verification-awaiting.js
+++ src/pages/login/verification-awaiting.js
@@ -10,42 +10,42 @@ function VerifyAwaiting() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
+    async function getPostsAsVerified(token_id) {
+        const URL = ConnectConfig.api_server.url + "/api/v1/post/recommended";
+
+        try {
+            const response = await fetch(URL, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token_id}`,
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (response.ok) {
+                let data = await response.json();
+                console.log(data);
+
+                dispatch(addPost(data.content));
+
+                navigate('/main');
+            } else {
+            }
+        } catch (error) {
+        }
+    }
+
     useEffect(() => {
         const tokenFromStorage = sessionStorage.getItem(ConnectConfig.api_server.session_token_id_name);
         if (tokenFromStorage) {
             setToken(tokenFromStorage);
         } else {
-            navigate('/');
+            //navigate('/');
         }
 
-        async function getPostsAsVerified() {
-            const URL = ConnectConfig.api_server.url + "/api/v1/post/recommended";
+        getPostsAsVerified(tokenFromStorage);
 
-            try {
-                const response = await fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': `Bearer ${tokenFromStorage}`,
-                        'Content-Type': 'application/json',
-                    }
-                });
-
-                if (response.ok) {
-                    let data = await response.json();
-                    console.log(data);
-
-                    dispatch(addPost(data.content));
-
-                    navigate('/main');
-                } else {
-                }
-            } catch (error) {
-            }
-        }
-
-        getPostsAsVerified();
-
-        const intervalId = setInterval(getPostsAsVerified, 30000);
+        const intervalId = setInterval(() => getPostsAsVerified(tokenFromStorage), 30000);
 
         return () => clearInterval(intervalId);
     }, []);
@@ -53,8 +53,31 @@ function VerifyAwaiting() {
     return (
         <div className="login-page">
             <div className="login-container">
-                <p className="login-display-text">Awaiting Verification</p>
-                <p className="login-display-text">Please check your email</p>
+                    <div className="bounce-container">
+                        <div className="bounce-1">A</div>
+                        <div className="bounce-2">W</div>
+                        <div className="bounce-3">A</div>
+                        <div className="bounce-4">I</div>
+                        <div className="bounce-5">T</div>
+                        <div className="bounce-6">I</div>
+                        <div className="bounce-7">N</div>
+                        <div className="bounce-8">G</div>
+                        <div className="bounchSpace"></div>
+                        <div className="bounce-9">V</div>
+                        <div className="bounce-10">E</div>
+                        <div className="bounce-11">R</div>
+                        <div className="bounce-12">I</div>
+                        <div className="bounce-13">F</div>
+                        <div className="bounce-14">I</div>
+                        <div className="bounce-15">C</div>
+                        <div className="bounce-16">A</div>
+                        <div className="bounce-17">T</div>
+                        <div className="bounce-18">I</div>
+                        <div className="bounce-19">O</div>
+                        <div className="bounce-20">N</div>
+                    </div>
+                <p className="login-header-text">Please check your email</p>
+                <p className="click-link-button" onClick={() => getPostsAsVerified(token)}>Force Refresh</p>
                 <Link className="login-links" to={'/'}>Back to login</Link>
             </div>
         </div>
