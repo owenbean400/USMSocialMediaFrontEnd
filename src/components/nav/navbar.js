6f8dc83cf80ff39825ef327a255d2d8c767fb702
owenbean400 <owenbean400@gmail.com>
2024-03-02 23:51:07 -0500

    Iteration of creating UI adding API endpoints

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -5,10 +5,8 @@ export default function NavBar() {
     return (<div>
         <nav className={styles.navbarContainer}>
             <div className={styles.navbarLeftContainer}>
-                <img className={styles.navbarLogo} src="https://d2jyir0m79gs60.cloudfront.net/college/logos_compressed/University_of_Southern_Maine.png"></img>
+                <img alt="USM Logo" className={styles.navbarLogo} src="https://d2jyir0m79gs60.cloudfront.net/college/logos_compressed/University_of_Southern_Maine.png"></img>
                 <Link className={styles.navbarLink} to={"/main"}>Feed</Link>
-                <Link className={styles.navbarLink} to={"/classmates"}>Classmates</Link>
-                <Link className={styles.navbarLink} to={"/groups"}>Groups</Link>
             </div>
             <div className={styles.navbarProfile}></div>
         </nav>
--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -1,10 +1,11 @@
 import styles from "./posts.module.css";
 import { useState } from 'react';
+import ConnectConfig from '../../config/connections.json';
 
 
 export default function Post(props) {
     const [showComments, setShowComments] = useState(false);
-    const [comments, setComments] = useState([{ name: "Owen Bean", title: "Greatest Person", content: "Wow so Cool!"}]);
+    const [comments, setComments] = useState(props.comments);
 
     function displayComment() {
         if (comments.length === 0 && !showComments) {
@@ -16,22 +17,46 @@ export default function Post(props) {
 
     function fetchComments() {
         // TODO: Fetch 5 comments or all if not enough
-        let fetchedComments = [
-            {
-                name: "Abby Pitcairn",
-                title: "CS Student",
-                content: "Amazing!"
-            },
-            {
-                name: "Michael Yattaw",
-                title: "CS Student",
-                content: "Hmmmmm....."
-            }
-        ];
+        let fetchedComments = [];
 
         const newComments = [...comments, ...fetchedComments];
 
         setComments(newComments);
+    }
+
+    async function likePost(post_id) {
+        const URL = ConnectConfig.api_server.url + "/api/v1/post/like";
+
+        const tokenFromStorage = sessionStorage.getItem(ConnectConfig.api_server.session_token_id_name);
+
+        try {
+            let likePost = {
+                targetId: post_id,
+            }
+
+            const response = await fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${tokenFromStorage}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(likePost)
+            });
+
+            if (response.ok) {
+                let data = await response.json();
+
+                console.log(data);
+            } else {
+                console.log("Status not ok");
+
+                let data = await response.json();
+
+                console.log(data);
+            }
+        } catch (error) {
+            console.log("Error")
+        }
     }
 
     return(
@@ -49,11 +74,11 @@ export default function Post(props) {
             <div className={styles.contentContainer}>{props.content}</div>
             <div className={styles.actionItemInfoContainer}>
                 <p className={styles.actionItemInfo}>{props.likes} Like{(props.likes) === 1 ? "" : "s"}</p>
-                <p className={styles.actionItemInfo} onClick={(e) => displayComment()}>{props.comments} Comment{(props.comments) === 1 ? "" : "s"}</p>
+                <p className={styles.actionItemInfo} onClick={(e) => displayComment()}>{props.comments.length} Comment{(props.comments.length) === 1 ? "" : "s"}</p>
             </div>
             <div className={styles.actionContainer}>
                 <ul className={styles.actionListContainer}>
-                    <li className={styles.actionList}>Like</li>
+                    <li className={styles.actionList} onClick={() => likePost(props.id)}>Like</li>
                     <div className={styles.barBlock}></div>
                     <li className={styles.actionList} onClick={(e) => displayComment()}>Comment</li>
                     <div className={styles.barBlock}></div>
@@ -62,12 +87,14 @@ export default function Post(props) {
             </div>
             {showComments && 
             <div className={styles.comments}>
-                <CommentWrite/>
+                <CommentWrite
+                    id={props.id}
+                    />
                 <div className={styles.commentsContainer}>
                     {comments.map((comment) => {
                         return <Comment
-                            name={comment.name}
-                            title={comment.title}
+                            name={comment.commenterFirstName + " " + comment.commenterLastName}
+                            title="{comment.title}"
                             content={comment.content} 
                         />
                     })}
@@ -97,11 +124,51 @@ function Comment(props) {
 }
 
 function CommentWrite(props) {
+    const [commentContent, setCommentContent] = useState('');
+
+    async function postComment() {
+        const URL = ConnectConfig.api_server.url + "/api/v1/post/comment";
+
+        const tokenFromStorage = sessionStorage.getItem(ConnectConfig.api_server.session_token_id_name);
+
+        try {
+            let post = {
+                id: props.id,
+                content: commentContent
+            }
+
+            const response = await fetch(URL, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${tokenFromStorage}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(post)
+            });
+
+            if (response.ok) {
+                let data = await response.json();
+
+                console.log(data);
+
+                setCommentContent('');
+            } else {
+                console.log("Status not ok");
+
+                let data = await response.json();
+
+                console.log(data);
+            }
+        } catch (error) {
+            console.log("Error")
+        }
+    }
+
     return(
         <div className={styles.commentWriteContainer}>
             <div className={styles.imageContainerComment}></div>
-            <input className={styles.commentInput} type="text" onChange={(e) => props.onChange(e.target.value)} />
-            <div className={styles.commentPost}>Post</div>
+            <input className={styles.commentInput} type="text" onChange={(e) => setCommentContent(e.target.value)} value={commentContent} />
+            <div className={styles.commentPost} onClick={() => postComment()}>Post</div>
         </div>
     )
 }
