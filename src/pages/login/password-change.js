bf984c5e60e0c3e76a7e64d8df48bbc8680e4757
Owen Bean <owenbean@host-0800-bcd074190a37.pool.net.maine.edu>
2024-03-06 14:21:30 -0500

    Fixed password reset not sending correct email address

--- src/pages/login/password-change.js
+++ src/pages/login/password-change.js
@@ -5,7 +5,7 @@ import Usmbutton from '../../components/button/usm-button';
 import ConnectConfig from '../../config/connections.json';
 
 function PasswordChange() {
-    let { emailAddress, resetToken } = useParams();
+    let { emailAddr, resetToken } = useParams();
     const [passInput, setPassInput] = useState('');
     const [confirmPassInput, setConfirmPassInput] = useState('');
     const [errMessage, setErrMessage] = useState('');
@@ -27,7 +27,7 @@ function PasswordChange() {
         }
         
         let credentials = {
-            email: emailAddress + "@maine.edu",
+            email: emailAddr + "@maine.edu",
             password: passInput
         }
 
@@ -55,7 +55,7 @@ function PasswordChange() {
           <div className="login-container">
                 <img className="usm-logo-top" src="https://imgs.search.brave.com/EPO1Pfw_I9IxOnuFLTFMf_y9eR2TpuWsZ_5t7ADh1Qg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91c20u/bWFpbmUuZWR1L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIyLzA3/L1VTTV9oZWFkZXJM/b2dvLnBuZw" width="380px" alt="USM Logo"></img>
                 <p className="login-error-text">{errMessage}</p>
-                <p className="login-display-text">Reset for {emailAddress}@maine.edu</p>
+                <p className="login-display-text">Reset for {emailAddr}@maine.edu</p>
                 <TextFieldPassword labelText="Password" onChange={handlePassChange}/>
                 <TextFieldPassword labelText="Confirm Password" onChange={handleConfirmPassChange}/>
                 <Usmbutton buttonText="Reset Password" onClick={registerAccount}/>
